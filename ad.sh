72d2960a27833f8f65ab7b252d723b05ffad8783
André <andrelopes.code@gmail.com>
2024-02-19 01:58:54 -0300

    aaaaaa

--- ad.sh
+++ ad.sh
@@ -24,12 +24,12 @@ first() {
     echo -e "${GREEN}Sistema atualizado.${RESET}"
     
     # Instalando dependencias
-      apt install build-essential libacl1-dev libattr1-dev libblkid-dev libgnutls-dev libreadline-dev python-dev libpam0g-dev python-dnspython gdb pkg-config libpopt-dev libldap2-dev dnsutils libbsd-dev docbook-xsl libcups2-dev nfs-kernel-server
+      apt install build-essential libacl1-dev libattr1-dev libblkid-dev libgnutls-dev libreadline-dev python-dev libpam0g-dev python-dnspython gdb pkg-config libpopt-dev libldap2-dev dnsutils libbsd-dev docbook-xsl libcups2-dev nfs-kernel-server -y > /dev/null
 
-      apt install samba samba-common smbclient cifs-utils samba-vfs-modules samba-testsuite samba-dbg samba-dsdb-modules cups cups-common cups-core-drivers nmap krb5-config winbind smbclient libnss-winbind libpam-winbind
+      apt install samba samba-common smbclient cifs-utils samba-vfs-modules samba-testsuite samba-dbg samba-dsdb-modules cups cups-common cups-core-drivers nmap winbind smbclient libnss-winbind libpam-winbind -y > /dev/null
 
     # Instalando kerberos
-    apt install expect > /dev/null
+    apt install expect -y > /dev/null
     expect -c "
     spawn apt install krb5-user krb5-config -y
     expect \"Reino por omissão do Kerberos versão 5:\"
