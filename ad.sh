0d50d82bd36c140166c99c1a2e0cea3db79c802c
André <andrelopes.code@gmail.com>
2024-02-19 02:08:47 -0300

    asdasdasd

--- ad.sh
+++ ad.sh
@@ -19,11 +19,13 @@ NAMESERVERS="${IP_ADDRESS} 8.8.8.8 1.1.1.1"
 
 first() {
   # Atualiza o sistema
-    echo -e "${YELLOW}Atualizando o sistema...${RESET}"
-    apt update && apt upgrade -y > /dev/null
+    echo -e "${YELLOW}Atualizando o sistema..."
+    apt update > /dev/null
+    apt upgrade -y > /dev/null
     echo -e "${GREEN}Sistema atualizado.${RESET}"
     
     # Instalando dependencias
+      echo -e "${YELLOW}Instalando dependencias..."
       apt install build-essential libacl1-dev libattr1-dev libblkid-dev libgnutls-dev libreadline-dev python-dev libpam0g-dev python-dnspython gdb pkg-config libpopt-dev libldap2-dev dnsutils libbsd-dev docbook-xsl libcups2-dev nfs-kernel-server -y > /dev/null
 
       apt install samba samba-common smbclient cifs-utils samba-vfs-modules samba-testsuite samba-dbg samba-dsdb-modules cups cups-common cups-core-drivers nmap winbind smbclient libnss-winbind libpam-winbind -y > /dev/null
@@ -41,18 +43,18 @@ first() {
     expect eof
     "
 
-  echo -e "${YELLOW}Dependencias instaladas.${RESET}"
+  echo -e "${GREEN}Dependencias instaladas.${RESET}"
 
 
   # Modifica o arquivo hosts
     sed -i "s/127\.0\.1\.1\t.*/127.0.1.1\t${HOSTNAME}\n${IP_ADDRESS}\t${HOSTNAME}.${DOMAIN}\t${HOSTNAME}/" /etc/hosts
-  echo -e "${YELLOW}Arquivo HOSTS alterado.${RESET}"
+  echo -e "${GREEN}Arquivo HOSTS alterado.${RESET}"
 
 
   # Altera o hostname
     sed -i "s/.*/${HOSTNAME}/" /etc/hostname
-    hostname -F /etc/hostname
-  echo -e "${YELLOW}HOSTNAME alterado.${RESET}"
+    hostname -F /etc/hostname 
+  echo -e "${GREEN}HOSTNAME alterado.${RESET}"
 
 
   # Configura a interface de rede
@@ -72,30 +74,32 @@ first() {
     dns-domain ${DOMAIN}\n\
     dns-search ${DOMAIN}" -e '11,$d' /etc/network/interfaces
     ifdown $INTERFACE_INTERNA && ifup $INTERFACE_INTERNA
-  echo -e "${YELLOW}Arquivo INTERFACES alterado.${RESET}"
+  echo -e "${GREEN}Arquivo INTERFACES alterado.${RESET}"
 
 
   # Renomear o arquivo smb.conf
   mv /etc/samba/smb.conf /etc/samba/smb.conf.bkp
-  echo -e "${YELLOW}Arquivo samba.conf renomeado.${RESET}"
+  echo -e "${GREEN}Arquivo samba.conf renomeado.${RESET}"
 
 
-  # Parar serviços necessarios
+  # Parar servicos necessarios
   systemctl stop smbd nmbd winbind systemd_resolved
   systemctl disable smbd nmbd winbind systemd_resolved
-  echo -e "${YELLOW}Serviços [smbd nmbd winbind systemd_resolved] parados.${RESET}"
+  echo -e "${RED}Servicos [smbd nmbd winbind systemd_resolved] parados.${RESET}"
 
+  # Provisionando o dominio
+  echo -e "${YELLOW}Preencha os campos...${RESET}"
   samba-tool domain provision --use-rfc2307 --use-xattr=yes --interactive
 
   # Move arquivo do kerberos
   mv /var/lib/samba/private/krb5.conf /etc/
-  echo -e "${YELLOW}krb5.conf movido.${RESET}"
+  echo -e "${GREEN}krb5.conf movido.${RESET}"
 
   # Iniciando e ativando samba-ad-dc
-  systemctl unmask samba-ad-dc
-	systemctl start samba-ad-dc
-	systemctl enable samba-ad-dc
-  echo -e "${YELLOW}samba-ad-dc ativado.${RESET}"
+  systemctl unmask samba-ad-dc > /dev/null
+	systemctl start samba-ad-dc > /dev/null
+	systemctl enable samba-ad-dc > /dev/null
+  echo -e "${GREEN}samba-ad-dc ativado.${RESET}"
 }
 
 
