21367e6f76c9399b481c8dd4198d304903e29463
André <andrelopes.code@gmail.com>
2024-02-24 03:38:50 -0300

    functions

--- configure.py
+++ configure.py
@@ -6,8 +6,9 @@ firewall = 'firewall.sh'
 allshfiles = (ad_dhcp, firewall)
 
 
-print("\033[1;33mEscolha os valores com atencao!\033[1;32m")
+print("\033[1;33mEscolha os valores com atencao!")
 # Definindo variaveis Gerais
+print("\033[1;35mConfigurações GERAIS:\033[1;32m")
 NEW_INTERFACE_INTERNA = input('interface interna: ').strip()
 NEW_INTERFACE_EXTERNA = input('interface externa: ').strip()
 NEW_DOMAIN = input('dominio: ').strip()
@@ -17,9 +18,11 @@ NEW_NETMASK = input('mascara: ').strip()
 NEW_GATEWAY = input('gateway: ').strip()
 NEW_BROADCAST = input('broadcast: ').strip()
 # Definindo variaveis do Firewall
+print("\033[1;35mConfigurações FIREWALL:\033[1;32m")
 IP_FIREWALL = input('IP: ').strip()
 HOSTNAME_FIREWALL = input('hostname: ').strip()
 # Definindo variaveis do AD-DHCP
+print("\033[1;35mConfigurações AD-DHCP:\033[1;32m")
 IP_AD = input('IP: ').strip()
 HOSTNAME_AD = input('hostname: ').strip()
 NEW_RANGE = input('Range [x.x.x.x x.x.x.x]: ').strip()
@@ -57,3 +60,10 @@ def files_change() -> None:
     subprocess.run(('sed', '-i', 's/acl sarg dst .*/acl sarg dst {}/'.format(IP_FIREWALL), '/home/scripts/docs/squid.conf'))
     subprocess.run(('sed', '-i', 's/acl localnet src .*/acl localnet src {}/'.format(NEW_NETWORK+'/24'), '/home/scripts/docs/squid.conf'))
     subprocess.run(('sed', '-i', 's/http_port .*:3128/http_port {}:3128/'.format(IP_FIREWALL), '/home/scripts/docs/squid.conf'))
+
+
+if __name__ == "__main__":
+    general_change()
+    firewall_change()
+    ad_dhcp_change()
+    files_change()
